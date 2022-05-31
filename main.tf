c9af0c890e26b229fade6fd0fc1547e156459f7b
Az DevOps <azdevops1@outlook.com>
2022-05-31 17:48:42 -0500

    updated tf to allow deletion

--- main.tf
+++ main.tf
@@ -11,9 +11,12 @@ terraform {
 }
 
 provider "azurerm" {
-  features {}
+  features {
+       resource_group {
+          prevent_deletion_if_contains_resources = false
+       }
+  }
 }
-
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "South Central US"
