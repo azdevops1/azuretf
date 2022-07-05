6e9cdd7f8a65cd1b591ff091b72915b19949826e
azdevops1 <48117603+azdevops1@users.noreply.github.com>
2022-07-05 17:54:02 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,3 +2,8 @@ variable "vm_size" {
   description = "Specifies the size of the virtual machine."
   type        = string
 }
+
+variable "azure_resource_group" {
+  description = "Specifies which Resrouce Group"
+  type        = string
+}
